e7b029efd146cc571a6acf8f07d810bf2b454107
Jacquot Thai <71291683+No1ceTea@users.noreply.github.com>
2022-11-02 14:38:12 +0100

    dev

--- api/index.php
+++ api/index.php
@@ -1,47 +1,50 @@
-<div class="example-wrapper">
-    <div>
-        <div>
-            <h2>All products</h2>
+<?php
+ini_set('display_errors', 0);
+
+require_once 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>PlanetScale PHP E-commerce</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+          crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+</head>
+
+<body>
+<div>
+    <nav class="navbar navbar-dark bg-primary sticky-top">
+        <a href="#" class="navbar-brand">
+            Product Management App
+        </a>
+
+        <div style="color: white; font-weight: bolder">
+            <p>Welcome!</p>
         </div>
-
-        <div>
-            <div class="container py-5">
-                <?php if(mysqli_num_rows(getProducts($mysqli)) > 0) : ?>
-                    <div class="row">
-                        <div class="col-lg-12 mx-auto">
-                            <ul class="list-group shadow">
-                                <?php
-                                $result = getProducts($mysqli);
-                                while ($row = $result->fetch_assoc()) {
-                                ?>
-                                <li class="list-group-item">
-                                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
-                                        <div class="media-body order-2 order-lg-1">
-                                            <h5 class="mt-0 font-weight-bold mb-2"><?= $row['name']; ?></h5>
-                                            <p class="font-italic text-muted mb-0 small"><?= $row['description']; ?></p>
-                                            <?php $categoryName = getCategoryName($mysqli, $row['category_id']); ?>
-                                            <p><small> Category: <b><?= $categoryName ?></b></small></p>
-                                        </div>
-                                        <img src="<?= $row['image']; ?>" alt="Generic placeholder image" width="200"
-                                             class="ml-lg-5 order-1 order-lg-2">
-                                    </div>
-                                </li>
-                                    <?php
-                                }
-                                ?>
-                            </ul>
-                        </div>
-                    </div>
-
-
-                <?php else: ?>
-
-                    <div class="row">
-                        <p>No products found</p>
-                    </div>
-
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
+    </nav>
 </div>
+
+<div>
+    <?php
+    include 'db.php';
+    include 'helper/functions.php';
+    include 'products.php';
+    ?>
+</div>
+
+</body>
+</html>
